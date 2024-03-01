17feb3aaccdd1bdc07d3b2210918c1dfd71e3c80
BYCODDE <anaraliev217@gmail.com>
2024-03-01 21:36:55 +0400

    desktop style done,responsive and js logic are not done yet!

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 let questions = document.querySelectorAll("#question h3");
 let orangeSvgs = document.querySelectorAll("#question svg");
-
+let isChanged = true;
 
 questions.forEach(function (question, index) {
   question.addEventListener("click", function () {
+    console.log(isChanged);
     let svg = orangeSvgs[index];
-    svg.style.transform = "rotate(180deg)";
+    if (isChanged) {
+      question.style.fontWeight = "700";
+      svg.style.transform = "rotate(180deg)";
+    } else {
+      svg.style.transform = "none";
+      question.style.fontWeight = "400";
+    }
+    isChanged = !isChanged;
   });
 });
 
 orangeSvgs.forEach(function (svg) {
   svg.addEventListener("click", function () {
-    svg.style.transform = "rotate(180deg)";
+    console.log(isChanged);
+    if (isChanged) {
+      svg.style.transform = "rotate(180deg)";
+      //   questions.style.fontWeight = "700";
+    } else {
+      svg.style.transform = "none";
+      //   questions.style.fontWeight = "400";
+    }
+    isChanged = !isChanged;
   });
 });
