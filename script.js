a8ea21417057475131d5e761992698011fa2e9d3
BYCODDE <anaraliev217@gmail.com>
2024-03-02 13:27:28 +0400

    same stuff left!

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 let questions = document.querySelectorAll("#question h3");
 let orangeSvgs = document.querySelectorAll("#question svg");
+let questionsDiv = document.querySelector("#question");
 let isChanged = true;
 
 questions.forEach(function (question, index) {
   question.addEventListener("click", function () {
-    console.log(isChanged);
+    let paragraph = document.createElement("p");
     let svg = orangeSvgs[index];
     if (isChanged) {
+      console.log(isChanged);
+      paragraph.textContent =
+        "No more than 2GB. All files in your account must fit your allotted storage space.";
+      questionsDiv.appendChild(paragraph);
       question.style.fontWeight = "700";
       svg.style.transform = "rotate(180deg)";
+      //       margin-top: 100px;
+      // padding-bottom: 30px;
+      //     padding-top: 19px;
     } else {
+      console.log(isChanged);
       svg.style.transform = "none";
       question.style.fontWeight = "400";
+      paragraph.parentNode.removeChild(paragraph);
     }
     isChanged = !isChanged;
   });
@@ -22,10 +32,10 @@ orangeSvgs.forEach(function (svg) {
     console.log(isChanged);
     if (isChanged) {
       svg.style.transform = "rotate(180deg)";
-      //   questions.style.fontWeight = "700";
+      questions.style.fontWeight = "700";
     } else {
       svg.style.transform = "none";
-      //   questions.style.fontWeight = "400";
+      questions.style.fontWeight = "400";
     }
     isChanged = !isChanged;
   });
