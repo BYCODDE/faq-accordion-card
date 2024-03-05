5895fc30d446d8a54ef725178a112893306578fa
BYCODDE <anaraliev217@gmail.com>
2024-03-05 16:51:09 +0400

    new js logic in process and responsive left!

--- script.js
+++ script.js
@@ -2,71 +2,57 @@ let questions = document.querySelectorAll("#question h3");
 let orangeSvgs = document.querySelectorAll("#question svg");
 let questionsDivs = document.querySelectorAll("#question");
 let lines = document.querySelectorAll("#line");
-let isChanged = true;
+let pTags = document.querySelectorAll("#main_p");
+// let isChanged = true;
+
 questions.forEach(function (question, index) {
   question.addEventListener("click", function () {
-    // let paragraph = document.createElement("p");
-    // paragraph.setAttribute("id", "myParagraph");
-    // let paragraphToRemove = document.getElementById("myParagraph");
-    // let svg = orangeSvgs[index];
-    // let questionDiv = questionsDivs[index];
-    // let line = lines[index];
-    // console.log(paragraph);
-    // if (isChanged && !paragraphToRemove) {
-    //   paragraph.textContent =
-    //     "No more than 2GB. All files in your account must fit your allotted storage space.";
-    //   questionDiv.appendChild(paragraph);
-    //   question.style.fontWeight = "700";
-    //   svg.style.transform = "rotate(180deg)";
-    //   question.style.paddingBottom = "30px";
-    //   // line.style.display = "block"; // Show the line
-    //   paragraph.style.marginTop = "35px";
-    //   line.style.marginTop = "95px";
-    // } else {
-    //   svg.style.transform = "none";
-    //   question.style.fontWeight = "400";
-    //   paragraphToRemove.remove();
-    //   question.style.paddingBottom = "0px";
-    //   line.style.marginTop = "50px";
-    //   // line.style.display = "none"; // Hide the line
-    // }
-    // isChanged = !isChanged;
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    pTags.forEach(function (paragraph) {
+      paragraph.style.display = "none";
+    });
+    let paragraphToShow = pTags[index];
+    paragraphToShow.style.display = "block";
   });
 });
+
+// let paragraph = document.createElement("p");
+// paragraph.setAttribute("id", "myParagraph");
+// let paragraphToRemove = document.getElementById("myParagraph");
+// let svg = orangeSvgs[index];
+// let questionDiv = questionsDivs[index];
+// let line = lines[index];
+// console.log(paragraph);
+// if (isChanged && !paragraphToRemove) {
+//   paragraph.textContent =
+//     "No more than 2GB. All files in your account must fit your allotted storage space.";
+//   questionDiv.appendChild(paragraph);
+//   question.style.fontWeight = "700";
+//   svg.style.transform = "rotate(180deg)";
+//   question.style.paddingBottom = "30px";
+//   // line.style.display = "block"; // Show the line
+//   paragraph.style.marginTop = "35px";
+//   line.style.marginTop = "95px";
+// } else {
+//   svg.style.transform = "none";
+//   question.style.fontWeight = "400";
+//   paragraphToRemove.remove();
+//   question.style.paddingBottom = "0px";
+//   line.style.marginTop = "50px";
+//   // line.style.display = "none"; // Hide the line
+// }
+// isChanged = !isChanged;
 
 // questions.forEach(function (question) {
-console.log(question);
-orangeSvgs.forEach(function (svg) {
-  svg.addEventListener("click", function () {
-    if (isChanged) {
-      svg.style.transform = "rotate(180deg)";
-      question.style.fontWeight = "700";
-    } else {
-      svg.style.transform = "none";
-      question.style.fontWeight = "400";
-    }
-    isChanged = !isChanged;
-  });
-});
+// orangeSvgs.forEach(function (svg) {
+//   svg.addEventListener("click", function () {
+//     if (isChanged) {
+//       svg.style.transform = "rotate(180deg)";
+//       question.style.fontWeight = "700";
+//     } else {
+//       svg.style.transform = "none";
+//       question.style.fontWeight = "400";
+//     }
+//     isChanged = !isChanged;
+//   });
+// });
 // });
